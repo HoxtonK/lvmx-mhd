105b9fd6ebf65d355259a968607e3292ce28408d
Elvmx <elvmx47@aliyun.com>
2020-04-22 17:11:26 +0800

    排行榜页面

--- src/api/cartoon.js
+++ src/api/cartoon.js
@@ -86,3 +86,23 @@ export const getTypeList = (subject, pageno = 1, pagesize = 20) => {
     })
   })
 }
+
+/**
+ * 获取排行榜数据
+ */
+export const getRankList = (ranktype, pageno = 1, pagesize = 20) => {
+  return request({
+    url: '/api/comic_v2/comicsrank',
+    method: 'POST',
+    params: {
+      apptype: 8,
+      appversion: '1.0',
+      channel: 'web-app'
+    },
+    data: format({
+      ranktype,
+      pageno,
+      pagesize
+    })
+  })
+}
