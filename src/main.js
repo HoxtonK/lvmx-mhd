470d8a503b29aa45ca8e4ba3f4b490d3aefbd9d4
Elvmx <elvmx47@aliyun.com>
2020-04-23 09:14:13 +0800

    路由懒加载

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import VueLazyload from 'vue-lazyload'
+
 import './assets/styles/base.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(VueLazyload, {
+  preload: 1.3,
+  error: require('./assets/logo.png'),
+  loading: require('./assets/logo.png'),
+  attempt: 1
+})
 
 // 全局定义一个过滤器。formatYi
 // Vue.filter('formatYi', (value) => {
